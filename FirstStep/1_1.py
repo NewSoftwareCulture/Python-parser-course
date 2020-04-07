fcd20f74d1119d58c42ada3a833b9ccb2c52ff08
Vlad <vlados.pochta@gmail.com>
2020-04-08 01:20:46 +0300

    Add 1_1

---
+++ FirstStep/1_1.py
@@ -0,0 +1,16 @@
+import requests
+url = 'https://stepik.org/media/attachments/lesson/209717/1.html'
+
+r = requests.get(url)  
+r.encoding = 'utf8'
+page = r.text
+
+count_c = page.count('C++')
+count_py = page.count('Python')
+
+# page.
+
+if count_c >= count_py:
+    print('C++')
+else:
+    print('Python')
