b67eb767102c7d586b722473af03f51badabe0fe
Vlad <vlados.pochta@gmail.com>
2020-04-08 16:44:57 +0300

    Update FirstStep, Add 2_1

--- FirstStep/1_4_2.py
+++ FirstStep/1_4_2.py
@@ -1,5 +1,18 @@
-import requests
-import re
+from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
+import re
 
-url = 'https://stepik.org/media/attachments/lesson/209719/2.html'
+url = 'https://stepik.org/media/attachments/lesson/209723/4.html'
+
+resp = urlopen(url) # скачиваем файл
+html = resp.read().decode('utf8') # считываем содержимое
+soup = BeautifulSoup(html, 'html.parser') # делаем суп
+table = str(soup.find('table'))
+
+numbers = re.findall(r'[0-9]{1,4}', table)
+
+sum = 0
+for i in numbers:
+    sum += int(i)
+
+print(sum)
--- FirstStep/1_4_3.py
+++ FirstStep/1_4_3.py
@@ -1,5 +1,17 @@
-import requests
-import re
+from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
+import re
 
-url = 'https://stepik.org/media/attachments/lesson/209719/2.html'
+url = 'https://stepik.org/media/attachments/lesson/209723/5.html '
+
+resp = urlopen(url) # скачиваем файл
+html = resp.read().decode('utf8') # считываем содержимое
+soup = BeautifulSoup(html, 'html.parser') # делаем суп
+table = soup.find_all('td')
+
+sum = 0
+
+for i in table:
+    sum += int(re.findall(r'[0-9]{1,4}', str(i))[0])
+
+print(sum)
